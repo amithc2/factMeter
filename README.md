d659285bc5aff12e5cf93329326486f5e105b7d5
amithc2 <amith.chivukula@gmail.com>
2020-05-30 16:20:31 -0500

    fixed some issues with anaconda hopefully

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # factMeter
 a "fact-checker" <br/>
 **To run/develop:** <br/>
-`conda env create -f hackdev.yml` <br/>
+`conda env create -f environment.yml` <br/>
 `python3 youtubeInt.py` <br/>
--- youtubeInt.py
+++ youtubeInt.py
@@ -10,11 +10,14 @@ def download_sub(video_url, lang='en'):
 		}
 	with youtube_dl.YoutubeDL(params) as yt:
 		yt.download([video_url])
-
+		return "%(name)s.vtt"
+def process_vtt(file_name):
+	print('ye old')
 if __name__ == '__main__':
 	url = 'https://www.youtube.com/watch?v=sCD9zjf_YRU'
 	# list_sub(url)
-	download_sub(url)
+	file_name = download_sub(url)
+	process_vtt(file_name)
 	client = Wit('WA2GGNG3O5LKUKKYTGQXFVMJFPE533A5')
 	resp = client.message('set an alarm tomorrow at 7am')
 	print(resp)
